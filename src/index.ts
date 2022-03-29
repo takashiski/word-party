bad32973a1d027671b169aa93369cb212cb37160
ASTIE@WebDog <78575902+astie-dog@users.noreply.github.com>
2022-03-29 22:25:58 +0900

    ギフトとNEWがセットだったときに金額の判定が難しくなってしまっていたのを修正

--- src/index.ts
+++ src/index.ts
@@ -47,11 +47,11 @@ function main(_op: Partial<WordPartyOptions> = {}) {
   ONE_SDK.subscribeComment((comments: Comment[]) => {
     const commentString = comments.map((comment) => {
       let com = striptags(comment.data.comment.replace(IMAGE_ALT, '$1'))
-      if (comment.service === 'youtube' && comment.data.paidText) {
-        com = `${comment.data.paidText} ${com}`
-      }
       if (comment.data.isFirstTime) {
         com = `__NEW__ ${com}`
+      }
+      if (comment.service === 'youtube' && comment.data.paidText) {
+        com = `${comment.data.paidText} ${com}`
       }
       if (comment.data.hasGift) {
         com = `__GIFT__ ${com}`
